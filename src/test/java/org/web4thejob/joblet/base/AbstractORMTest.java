413e385c9d5bf450e0bed7a3f1da289544b92073
Veniamin Isaias <bissaias@hotmail.com>
2013-07-30 20:57:05 +0300

    Copyright notice

--- src/test/java/org/web4thejob/joblet/base/AbstractORMTest.java
+++ src/test/java/org/web4thejob/joblet/base/AbstractORMTest.java
@@ -1,21 +1,21 @@
-/*
- * Copyright (c) 2012 Veniamin Isaias.
- *
- * This file is part of web4thejob.
- *
- * Web4thejob is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or any later version.
- *
- * Web4thejob is distributed in the hope that it will be useful,
+/*******************************************************************************
+ * Copyright (c) 2013 Veniamin Isaias.
+ * 
+ * This file is part of joblet-builder.
+ * 
+ * Joblet-builder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Joblet-builder is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with web4thejob.  If not, see <http://www.gnu.org/licenses/>.
- */
-
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with joblet-builder.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package org.web4thejob.joblet.base;
 
 import org.junit.Before;
