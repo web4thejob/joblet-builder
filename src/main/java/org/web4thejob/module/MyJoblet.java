310310f2879e29c83619a340402df1c21a42e876
bissaias <bissaias@hotmail.com>
2014-09-14 18:57:29 +0300

    jdk 1.5

--- src/main/java/org/web4thejob/module/MyJoblet.java
+++ src/main/java/org/web4thejob/module/MyJoblet.java
@@ -1,18 +1,18 @@
 /*******************************************************************************
  * Copyright (c) 2013 Veniamin Isaias.
- * 
+ *
  * This file is part of joblet-builder.
- * 
+ *
  * Joblet-builder is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Joblet-builder is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with joblet-builder.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
@@ -28,43 +28,43 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MyJoblet extends AbstractJoblet {
+    private static final String BASE_PACKAGE = "com.example"; //<-- Set you base package here
 
-	@Override
-	public int getOrdinal() {
-		return 99;
-	}
+    public int getOrdinal() {
+        return 99;
+    }
 
-	@Override
-	public boolean isInstalled() {
-		return true;
-	}
+    public boolean isInstalled() {
+        return true;
+    }
 
-	@Override
-	public String[] getSchemas() {
-		return new String[] { "example" };
-	}
+    public String[] getSchemas() {
+        return new String[]{"example"};
+    }
 
-	@Override
-	public List<Resource> getResources() {
-		List<Resource> resources = new ArrayList<Resource>();
+    public List<Resource> getResources() {
+        List<Resource> resources = new ArrayList<Resource>();
 
-		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-		try {
-			for (Resource resource : resolver
-					.getResources("classpath*:com/example/**/*.hbm.xml")) {
-				resources.add(resource);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        try {
+            for (Resource resource : resolver
+                    .getResources("classpath*:" + packageToPath() + "/**/*.hbm.xml")) {
+                resources.add(resource);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
 
-		return resources;
-	}
+        return resources;
+    }
 
-	@Override
-	public String getBasePackage() {
-		return "com.example";
-	}
+    public String getBasePackage() {
+        return BASE_PACKAGE;
+    }
+
+    private static String packageToPath() {
+        return BASE_PACKAGE.replaceAll("\\.", "/");
+    }
 
 }
--- src/test/java/org/web4thejob/joblet/ORMTest.java
+++ src/test/java/org/web4thejob/joblet/ORMTest.java
@@ -1,39 +1,58 @@
 /*******************************************************************************
  * Copyright (c) 2013 Veniamin Isaias.
- * 
+ *
  * This file is part of joblet-builder.
- * 
+ *
  * Joblet-builder is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Joblet-builder is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with joblet-builder.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package org.web4thejob.joblet;
 
 import org.junit.Test;
+import org.springframework.transaction.annotation.Transactional;
+import org.web4thejob.context.ContextUtil;
 import org.web4thejob.joblet.base.AbstractORMTest;
+import org.web4thejob.module.MyJoblet;
+import org.web4thejob.orm.Path;
+import org.web4thejob.orm.query.Condition;
+import org.web4thejob.orm.query.Query;
+import org.web4thejob.security.UserIdentity;
 
 /**
- * <p>
+ * <p/>
  * Type your tests here.
- * <p>
+ * <p/>
  * Switch to the <strong>example</strong> branch or visit <a href=
  * "https://github.com/web4thejob/joblet-builder/blob/example/src/test/java/org/web4thejob/joblet/ORMTest.java"
  * >github</a> for an example.
  */
 public class ORMTest extends AbstractORMTest {
 
-	@Test
-	public void test1() {
-		// Type your test here.
-	}
+    @Test @Transactional
+    public void test1() {
+        Query query = ContextUtil.getEntityFactory().buildQuery(UserIdentity.class);
+        query.addCriterion(new Path(UserIdentity.FLD_CODE), Condition.EQ, "new_user");
+        UserIdentity userIdentity = ContextUtil.getDRS().findUniqueByQuery(query);
+        if (userIdentity != null) {
+            ContextUtil.getDWS().delete(userIdentity);
+        }
+
+        userIdentity = ContextUtil.getEntityFactory().buildUserIdentity();
+        userIdentity.setCode("new_user");
+        userIdentity.setPassword("xxxx");
+        userIdentity.setLastName("Lastname");
+        userIdentity.setFirstName("Firstname");
+        ContextUtil.getDWS().save(userIdentity);
+    }
 
 }
